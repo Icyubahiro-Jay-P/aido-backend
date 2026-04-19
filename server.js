1c9c407cc79ac132bce2057c54705e52932cbe0a
Icyubahiro-Jay-P <icyubahiro1980@gmail.com>
2026-04-19 13:05:24 +0200

    first-commit

--- server.js
+++ server.js
@@ -19,8 +19,11 @@ app.use(express.json());
 app.use(
   cors({
     // origin: "https://aido-group-company-ltd.vercel.app/",
-    origin: "http://localhost:5173",
-    credentials: true
+    origin: [
+      "http://localhost:5173",
+      "https://aido-group-company-ltd.vercel.app/",
+    ],
+    credentials: true,
   }),
 );
 app.use(cookieParser());
@@ -31,15 +34,15 @@ app.use("/api/sales", saleRoutes);
 app.use("/api/reports", reportRoutes);
 app.use("/api/clients", clientRoutes);
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Inventory Management API is running' });
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Inventory Management API is running" });
 });
 
 // Global error handler
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(err.status || 500).json({ 
-    message: err.message || 'Internal Server Error' 
+  res.status(err.status || 500).json({
+    message: err.message || "Internal Server Error",
   });
 });
 
