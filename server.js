206171820e836cdfb47d54a853f98d41c691d8c7
Icyubahiro-Jay-P <151947328+Icyubahiro-Jay-P@users.noreply.github.com>
2026-04-19 13:10:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "*"
+    origin: "*",
     // origin: [
     //   "http://localhost:5173",
     //   "https://aido-group-company-ltd.vercel.app/",
